18a2d6b62ec4bba21e55ad08e633f10c21338d13
26Vilela05 <victor.vilela011@gmail.com>
2025-09-18 07:42:18 -0300

    Projeto Pizzaria

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,10 @@
-import { useState } from "react";
-import poke from './assets/pokeapi_256.png'
-
-function App() {
-
-
-
-  // var nome = "Victor";
-
-  const Formulario = (props) => {
-
-    const [nome, setNome] = useState("Jesus")
-
-
-    return (
-      <div>
-        <input
-          className="nome"
-          name="nome"
-          onChange={(e) => { setNome(e.target.value) }}
-          placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
-          type="text" />
-
-        <button
-          className="Botao"
-          onClick={() => { alert(nome) }}>
-            CLIQUE AQUI
-        </button>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <img src={poke} style={{width:200, height:100}} />
-      <img src={poke} style={{width:200, height:100}} />
-      <h3>Pizzaria 2F</h3>
-      <Formulario sombra="digite seu nome..." />
-      <Formulario sombra="digite seu e-mail" />
-      <Formulario sombra="(99)90000-0000" />
-      <Formulario />
-      <Formulario />
-      <Formulario />
-    </div>
+const App = () => {
+  return(
+    <>
+      <h1>Projeto Pizzaria</h1>
+    </>
 
   )
-}
 
+}
 export default App
