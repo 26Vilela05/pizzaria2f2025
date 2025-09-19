04f19a67fb84a40604e1664b8c73fa482d3fd240
26Vilela05 <victor.vilela011@gmail.com>
2025-09-19 08:32:03 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,14 +1,13 @@
+import axios from "axios"
+
 const Produtos = () => {
-    const pizzas = [
-            'Pizza de Muçarela',
-            'Pizza de Calabresa',
-            'Pizza de Frango',
-            'Pizza Portuguesa'
 
-    ]
-
+    axios.get("http://172.19.0.49/pizzariateste/api/v1/index")
+    .then((response) => {
+        console.log(response.data)
+    })
+  
     const ListaPizzas = pizzas.map(pizza => <li>{pizza}</li>)
-
 
     return (
         <div>
